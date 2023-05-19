1827c60fbb40fdb9a3b4980243e2cf90539902ce
Kurt <kurtjonathangozano@gmail.com>
2023-05-19 15:08:51 +0800

    updated contacts code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
-import Axios from 'axios'
+import React from "react"
+// import React, { useEffect, useState } from "react";
+// import Axios from 'axios'
 
 const Contact = () => {
-    const [senderEmail, setSenderEmail] = useState('')
-    const [senderSubject, setSenderSubject] = useState('')
-    const [senderMessage, setSenderMessage] = useState('')
-    const [senderList, setSenderList] = useState([])
+    // const [senderEmail, setSenderEmail] = useState('')
+    // const [senderSubject, setSenderSubject] = useState('')
+    // const [senderMessage, setSenderMessage] = useState('')
+    // const [senderList, setSenderList] = useState([])
 
-    const submitButton = () => {
-        Axios.post('http://localhost:3001/api/insert', {
-            senderEmail: senderEmail,
-            senderSubject: senderSubject,
-            senderMessage: senderMessage
-        })
+    // const submitButton = () => {
+    //     Axios.post('http://localhost:3001/api/insert', {
+    //         senderEmail: senderEmail,
+    //         senderSubject: senderSubject,
+    //         senderMessage: senderMessage
+    //     })
 
-    }
+    // }
 
     return (
         <div>
@@ -35,9 +36,9 @@ const Contact = () => {
                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                    placeholder="example@gmail.com"
                                    required 
-                                   onChange={(e) => {
-                                    setSenderEmail(e.target.value)
-                                   }}
+                                //    onChange={(e) => {
+                                //     setSenderEmail(e.target.value)
+                                //    }}
                                    />
                         </div>
                         <div>
@@ -48,9 +49,9 @@ const Contact = () => {
                                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                    placeholder="Let me know how I can help you"
                                    required
-                                   onChange={(e) => {
-                                    setSenderSubject(e.target.value)
-                                   }}
+                                //    onChange={(e) => {
+                                //     setSenderSubject(e.target.value)
+                                //    }}
                                    />
                         </div>
                         <div className="sm:col-span-2">
@@ -60,15 +61,16 @@ const Contact = () => {
                             <textarea id="message" rows="6"
                                       className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                       placeholder="Leave a comment..."
-                                      onChange={(e) => {
-                                        setSenderMessage(e.target.value)
-                                       }}
+                                    //   onChange={(e) => {
+                                    //     setSenderMessage(e.target.value)
+                                    //    }}
                                       >
                             </textarea>
                         </div>
                         <button type="submit"
                                 className="py-3 px-5 text-sm font-medium text-center text-black dark:text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                                onClick={submitButton}>
+                                // onClick={submitButton}
+                                >
                             <a href={"mailto:kurtjonathangozano@gmail.com"}>
                                 Send message
                             </a>
