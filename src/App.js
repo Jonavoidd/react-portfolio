873bbe3a24d9d171af780f8e5c762a43b5336190
Kurt <kurtjonathangozano@gmail.com>
2023-05-19 14:09:02 +0800

    first commit for this one

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import MainLayout from "./layout/MainLayout";
+import LandingPage from "./components/LandingPage";
+import '@ionic/react/css/core.css';
+import { setupIonicReact } from "@ionic/react";
+
+setupIonicReact()
+
+function App() {
+    return (
+		<MainLayout>
+            <LandingPage />
+        </MainLayout>
+    );
+}
+
+export default App;
