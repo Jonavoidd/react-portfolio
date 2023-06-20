a2dae3e9ed0e9cb1bdec7aad151d71d004d6b265
Kurt <kurtjonathangozano@gmail.com>
2023-06-20 14:42:17 +0800

    updated text position in projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -41,18 +41,19 @@ const MyCard = ({ title, bgImage, url }) => {
     <Link to={url} target="__blank" className={"justify-center items-center"}>
       <div
         className={
-          "relative h-full block md:w-1/2 my-24 bg-white rounded-lg shadow m-auto overflow-hidden"
+          "relative h-full block md:w-1/2 mt-12 bg-white rounded-lg shadow m-auto overflow-hidden"
         }
       >
         <img src={bgImage} alt="bg" className={"w-full h-full rounded-lg object-cover transform hover:scale-125 transition duration-1000 ease-out"} />
-        <h1
+        
+      </div>
+      <h1
           className={
-            "absolute bottom-0 justify-center text-xl font-bold text-black w-full text-center"
+            "mb-12 justify-center text-xl font-bold text-black dark:text-white w-full text-center"
           }
         >
           {title}
         </h1>
-      </div>
     </Link>
   );
 };
